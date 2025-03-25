035dadaaf9e53fbfea730b2a3daa79c4f17fc89d
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2025-03-25 11:58:18 +0530

    git push

--- components/sections/Products.tsx
+++ components/sections/Products.tsx
@@ -15,7 +15,7 @@ const products = [
     name: "QuickGig",
     icon: <Briefcase className="text-blue-600 w-10 h-10" />,
     bgColor: "bg-blue-100",
-    href: "https://dot-opal.vercel.app/",
+    href: "https://dot-fiverr.vercel.app/",
   },
   {
     id: 3,
